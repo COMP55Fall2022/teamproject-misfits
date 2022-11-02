edfbe3bd6b4ac4e838effca3efb115b2ddefdefb
Geoffrey <g_gonzales5@u.pacific.edu>
2022-11-02 15:44:47 -0700

    creating Entity Class, adding some attributes

--- src/main/java/edu/pacific/comp55/starter/Entity.java
+++ src/main/java/edu/pacific/comp55/starter/Entity.java
@@ -1,5 +1,31 @@
 package edu.pacific.comp55.starter;
 
+//Base values for the entity class
 public class Entity {
+	int health;
+	int attackDamage;
+	int moveSpeed;
+	int jumpPower=10;
+	boolean isParrying=false;
+	boolean isAttacking=false;
+	boolean attackDebounce=false;
+	
+	//ENTITY CLASS CONSTRUCTOR
 
+	public Entity(int HP,int AD,int MS,int JP) {
+		//setting up the entity's attributes
+		health = HP;
+		attackDamage = AD;
+		moveSpeed = MS;
+		jumpPower = JP;
+	}
+	
+	public void drawPlayermodel() {
+		
+	}
+
+	
+	
 }
+
+
