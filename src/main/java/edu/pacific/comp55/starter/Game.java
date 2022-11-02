5f055fec14293b42d98e40849de90d4cd5a73e5c
Santi <85santig@gmail.com>
2022-11-02 16:46:37 -0700

    added keyPressed and pausegame

--- src/main/java/edu/pacific/comp55/starter/Game.java
+++ src/main/java/edu/pacific/comp55/starter/Game.java
@@ -1,26 +1,45 @@
 package edu.pacific.comp55.starter;
 
+import java.awt.event.KeyEvent;
 import java.util.Scanner;
 
 public class Game {
+	boolean playing = false;
+	boolean pause = false;
+	boolean resume = false;
+	
+	
 	Level level;
 	String wantPlay;
 	Scanner playerInput = new Scanner(System.in);
 
 	
 	public void startGame(){
-		System.out.println("Would you like to play?");
+		System.out.println("Would you like to play? Press (Y) for yes, or (N) for no.");
 		wantPlay = playerInput.next();
 		
+		if(wantPlay.equals("Y") || wantPlay.equals("y")) {
+			playing = true;
+		}
 		
 	}
 	
 	public void pauseGame() {
+		pause = !pause;
 		
+		if(pause) {
+			System.out.println("Game paused");
+		}
 	}
 	
-	public void resumeGame () {
+	public void keyPressed(KeyEvent e) {
+		int keycode = e.getKeyCode();
 		
+		switch(keycode) {
+		case KeyEvent.VK_P -> pauseGame();
+		
+		}
+	
 	}
 	
 	
