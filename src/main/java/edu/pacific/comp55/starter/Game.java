8637d07b9d4202a334ff0efc7411c14ca8c37381
Santi <85santig@gmail.com>
2022-11-02 16:08:49 -0700

    started Game class

--- src/main/java/edu/pacific/comp55/starter/Game.java
+++ src/main/java/edu/pacific/comp55/starter/Game.java
@@ -1,5 +1,28 @@
 package edu.pacific.comp55.starter;
 
-public class Game {
+import java.util.Scanner;
 
+public class Game {
+	Level level;
+	String wantPlay;
+	Scanner playerInput = new Scanner(System.in);
+
+	
+	public void startGame(){
+		System.out.println("Would you like to play?");
+		wantPlay = playerInput.next();
+		
+		
+	}
+	
+	public void pauseGame() {
+		
+	}
+	
+	public void resumeGame () {
+		
+	}
+	
+	
+	
 }
