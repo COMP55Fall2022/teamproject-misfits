0f33f7c3790443ee75a03f71f8c323718b82ef7a
Kprasad03 <K_prasad2@gmail.com>
2022-11-02 16:11:40 -0700

    creating level class, and adding values

--- src/main/java/edu/pacific/comp55/starter/Level.java
+++ src/main/java/edu/pacific/comp55/starter/Level.java
@@ -1,5 +1,19 @@
 package edu.pacific.comp55.starter;
 
-public class Level {
+import acm.graphics.*;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
+public abstract class Level implements ActionListener {
+	int currentLevel;
+	int enemiesKilled;
+	int enemiesRemaining;
+	int damageDealt;
+	//player addPlayer;
+	int windowHeight;
+	int windowWidth;
+	Timer addEnemy = new Timer(1000, this);
+	Timer attack = new Timer(1000, this);
+	
+	
 }
