e6b23212dccfc938d6188b71b7d8c81f4adc2b03
Kprasad03 <K_prasad2@gmail.com>
2022-11-02 16:29:18 -0700

    added template of functions to the level class

--- src/main/java/edu/pacific/comp55/starter/Level.java
+++ src/main/java/edu/pacific/comp55/starter/Level.java
@@ -1,19 +1,64 @@
 package edu.pacific.comp55.starter;
 
-import acm.graphics.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
 import javax.swing.*;
 
-public abstract class Level implements ActionListener {
+public class Level implements ActionListener {
 	int currentLevel;
 	int enemiesKilled;
 	int enemiesRemaining;
 	int damageDealt;
-	//player addPlayer;
+	//private Level addPlayer;
 	int windowHeight;
 	int windowWidth;
 	Timer addEnemy = new Timer(1000, this);
 	Timer attack = new Timer(1000, this);
+	
+	public void keyPressed(KeyEvent e) {
+		
+	}
+	
+	public void keyReleased(KeyEvent e) {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void attack() {
+		
+	}
+	
+	public void addAnEnemy() {
+		
+	}
+	
+	public void moveAllEnemies() {
+		
+	}
+	
+	public void resetLevelStats() {
+		
+	}
+	
+	public void populateEnemyArray() {
+		
+	}
+	
+	public void clearEnemyArray() {
+		
+	}
+	
+	public static void run(String[] args) {
+		
+	}
+
 	
 	
 }
