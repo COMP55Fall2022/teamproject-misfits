51ee6615e4d872655163de2876d896040b7e341f
Santi <85santig@gmail.com>
2022-11-02 19:37:32 -0700

    made corrections to try/catch

--- src/main/java/edu/pacific/comp55/starter/Game.java
+++ src/main/java/edu/pacific/comp55/starter/Game.java
@@ -4,41 +4,45 @@ import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.Scanner;
 
+
+
 public class Game {
 	boolean playing = false;
 	boolean pause = false;
 	boolean resume = false;
 	boolean running = false;
-	
+	private Scanner playerInput;
+
 	Level level;
 	String wantPlay;
-	Scanner playerInput = new Scanner(System.in);
+	
 
 	
 	public void startGame(){
-		
 		running = true;
-		
+		playerInput = new Scanner(System.in);
 		System.out.println("Would you like to play?");
-		
 		int playerChoice = 0;
 		
 		do {
 			try {
-				System.out.println("Press (1) for yes, or (2) for no)");
+				System.out.println("Press 1 for yes, or 2 for no)");
 				playerChoice = playerInput.nextInt();
 			}
 			catch (Exception e){
-				System.out.println("That is not an option. Please select (1) to play or (2) to exit.");
+				System.out.println("That is not an option. Please select 1 to play or 2 to exit.");
 				playerInput = new Scanner(System.in);
 				
 			}
-		} while(playerChoice ==1 ); {
-			playing = true;
+		} while(playerChoice <1 || playerChoice >2 );{
+			if(playerChoice ==1)
+				System.out.println("play game");
+			
+				else if(playerChoice ==2) {
+			System.out.println("you quit");
 		}
-		while(playerChoice ==2 );{
-			playing = false;
 		}
+	
 	}
 	
 	
@@ -65,5 +69,7 @@ public class Game {
 	public static void main(String[] args) throws IOException {
 		Game game = new Game();
 		game.startGame();
+		
+		
 	}
 }
