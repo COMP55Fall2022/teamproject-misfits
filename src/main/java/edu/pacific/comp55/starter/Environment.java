986e63223453b21440a61156ef774fc24ab30df4
akhojaza <akhojaza@STK-ECS-JR3Z243.stk.pacific.edu>
2022-11-04 16:39:13 -0700

    placeholder for background images (it could be some mountains or something, but right now its just a cloud img) - Arsalan

--- src/main/java/edu/pacific/comp55/starter/Environment.java
+++ src/main/java/edu/pacific/comp55/starter/Environment.java
@@ -1,13 +1,28 @@
 package edu.pacific.comp55.starter;
+import acm.graphics.GImage;
+import acm.program.GraphicsProgram;
 
-public class Environment {
-	
-	
+import acm.graphics.*;
 
+
+	public class Environment extends GraphicsProgram{
+		
+		GImage cloud = new GImage("media/cumulus-cloud-3.jpg",0,0);
+
+		public void init() {
+			setSize(1920,1080);
+			requestFocus();
+		}
+		
+		public void run() {
+			cloud.setSize(1920,1080);
+			add(cloud);
+		}
+		
+		
 	
 	
 	public void setBackground( ){
-		
 		
 	}
 	
@@ -21,4 +36,10 @@ public class Environment {
 		
 	}
 	
+	
+	public static void main(String[] args) {
+		new Environment().start();
+	}
+
 }
+
