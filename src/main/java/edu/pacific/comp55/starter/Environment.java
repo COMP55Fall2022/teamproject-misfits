d8e8a7ec2b2ccbef1e9f0f2229a3bb52a9b95577
akhojaza <akhojaza@STK-ECS-JR3Z243.stk.pacific.edu>
2022-11-04 16:19:43 -0700

    This is Arsalan

--- src/main/java/edu/pacific/comp55/starter/Environment.java
+++ src/main/java/edu/pacific/comp55/starter/Environment.java
@@ -1,5 +1,24 @@
 package edu.pacific.comp55.starter;
 
 public class Environment {
+	
+	
 
+	
+	
+	public void setBackground( ){
+		
+		
+	}
+	
+	
+	public void setClouds( ) {
+		
+		
+	}
+	
+	public void moveClouds( ) {
+		
+	}
+	
 }
