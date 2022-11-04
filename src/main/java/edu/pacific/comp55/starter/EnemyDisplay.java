601efa9df857ff62f5bb785318838f16458274f3
Kprasad03 <K_prasad2@gmail.com>
2022-11-04 16:48:55 -0700

    Started creating the barebones code for the EnemyDisplays

--- src/main/java/edu/pacific/comp55/starter/EnemyDisplay.java
+++ src/main/java/edu/pacific/comp55/starter/EnemyDisplay.java
@@ -1,19 +1,32 @@
 package edu.pacific.comp55.starter;
 import acm.program.GraphicsProgram;
+import acm.graphics.*;
 
 public abstract class EnemyDisplay extends GraphicsProgram{
 	
+	
 	public void drawHUD() {
+		//GImage HealthBar = new GImage();
+		//add(HealthBar);
+		
+		//GImage EnemyIcon = new GImage();
+		//add(EnemyIcon);
 		
 	}
 	
 	public int decreaseBar() {
+		//GImage BarParticle = new GImage();
+		//add(BarParticle);
+		
+		
 		return 0;
+		
 		
 	}
 	
 	public void drawStatusEffect() {
-		
+		//GImage StatusEffect = new GImage();
+		//add(StatusEffect);
 	}
 
 }
