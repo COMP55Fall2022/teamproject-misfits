cceda0081d262981eb40d06e8b4b578c655f9cfa
Kprasad03 <K_prasad2@gmail.com>
2022-11-04 16:03:10 -0700

    Created functions for the Popups class

--- src/main/java/edu/pacific/comp55/starter/Popups.java
+++ src/main/java/edu/pacific/comp55/starter/Popups.java
@@ -1,5 +1,34 @@
 package edu.pacific.comp55.starter;
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class Popups {
+public class Popups extends GraphicsProgram{
+	
+	public void drawLossScreen() {
+		//GImage image = new GImage();
+		//add(image);
+	}
+	
+	public void drawWinScreen() {
+		//GImage image = new GImage();
+		//add(image);
+	}
+	
+	public void drawBeginningTitle() {
+		//GImage image = new GImage();
+		//add(image);
+	}
+	
+	public void drawSummaryScreen() {
+		
+	}
 
+	@Override
+	public void run() {
+
+		
+	}
+	
+	
+	
 }
