e42b4ce8f5feb636740eac57ec782b8b233050d2
Santi <85santig@gmail.com>
2022-11-13 05:52:27 -0800

    added goback button to help menu

--- src/main/java/edu/pacific/comp55/starter/HUD.java
+++ src/main/java/edu/pacific/comp55/starter/HUD.java
@@ -9,21 +9,27 @@ public class HUD extends Display {
 	 private Game program; // you will use program to get access to 
 	  //all of the GraphicsProgram calls
 	  
-	  private GImage img; private GParagraph para;
+	  private GImage img; 
+	  private GParagraph para;
 	  
 	 public HUD(Game app) { this.program = app; img = new
-	 GImage("robot head.jpg", 100, 100); para = new
-	  GParagraph("welcome\nto my\nsecret room!", 150, 300);
+	 GImage("robot head.jpg", 100, 100);
+	 para = new GParagraph("welcome\nto my\nsecret room!", 150, 300);
 	  para.setFont("Arial-24"); }
 	  
-	  @Override public void showContents() { program.add(img); program.add(para); }
+	  @Override public void showContents() { 
+		  program.add(img); 
+		  program.add(para); }
 	  
-	  @Override public void hideContents() { program.remove(img);
+	  @Override public void hideContents() { 
+		  program.remove(img);
 	  program.remove(para); }
 	  
 	  @Override public void mousePressed(MouseEvent e) {
-	  para.setText("you need\nto click\non the eyes\nto go back"); GObject obj =
-	  program.getElementAt(e.getX(), e.getY()); if (obj == img) {
+	  para.setText("you need\nto click\non the eyes\nto go back");
+	  GObject obj =
+	  program.getElementAt(e.getX(), e.getY());
+	  if (obj == img) {
 	  program.switchToMenu(); } }
 
 	@Override
--- src/main/java/edu/pacific/comp55/starter/Help.java
+++ src/main/java/edu/pacific/comp55/starter/Help.java
@@ -1,5 +1,6 @@
 package edu.pacific.comp55.starter;
 
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 
 
@@ -9,37 +10,42 @@ import acm.graphics.GObject;
 
 public class Help extends Display {
 
-	 private Game program; // you will use program to get access to 
+	 private Game helpProgram; // you will use program to get access to 
 	  //all of the GraphicsProgram calls
 	  
 	  private GImage img; 
-	  private GParagraph para;
+	  private GParagraph para; 
+	  private GButton goBack;
 	  
 	 public Help(Game app) { 
-	this.program = app; 
-	//img = new GImage("robot head.jpg", 100, 100); 
-	para = new GParagraph("Welcome to the Help Menu.", 150, 300);
-	  para.setFont("Arial-24"); }
+		 this.helpProgram = app; 
+	//	 img = new GImage("robot head.jpg", 100, 100);
+		 para = new GParagraph("Welcome to the help Menu", 250, 100);
+		 para.setFont("Arial-24"); 
+
+		 goBack = new GButton("Return to Menu", 300, 200, 200, 50);
+		 goBack.setFillColor(Color.yellow);
+		 }
 	  
-	  @Override public void showContents() {
-		//  program.add(img); 
-		  program.add(para); }
+	  @Override public void showContents() { 
+		  helpProgram.add(goBack); 
+		  helpProgram.add(para); }
 	  
-	  @Override public void hideContents() {
-		  //program.remove(img);
-	  program.remove(para); }
+	  @Override public void hideContents() { 
+		  helpProgram.remove(goBack);
+		  helpProgram.remove(para); }
 	  
 	  @Override public void mousePressed(MouseEvent e) {
-	 //para.setText("you need\nto click\non the eyes\nto go back"); 
-	 // GObject obj =	  program.getElementAt(e.getX(), e.getY()); if (obj == img) {
-	  //program.switchToMenu(); } 
-		  }
+	  GObject obj = helpProgram.getElementAt(e.getX(), e.getY());
+	  if (obj == goBack) {
+	  helpProgram.switchToMenu(); } }
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
+	  @Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+
 	 }
 
 
