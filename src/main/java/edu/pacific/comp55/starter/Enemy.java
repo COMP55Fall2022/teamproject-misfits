49fd98714a7ef9153032b902a6ca5e16ceabdaa9
Geoffrey <g_gonzales5@u.pacific.edu>
2022-11-14 15:34:38 -0800

    Enemy

--- src/main/java/edu/pacific/comp55/starter/Enemy.java
+++ src/main/java/edu/pacific/comp55/starter/Enemy.java
@@ -1,5 +1,10 @@
 package edu.pacific.comp55.starter;
 
-public class Enemy {
+public class Enemy extends Entity {
+
+	public Enemy(int HP, int AD, int MS, int JP, String playerModel) {
+		super(HP, AD, MS, JP, playerModel);
+		// TODO Auto-generated constructor stub
+	}
 
 }
