fe49c26c8b5715479d0375496b4c64c7491b87a6
ArsalanKhojazada <a_khojazada@u.pacific.edu>
2022-11-16 11:06:49 -0800

    Okay I'am making the cloud move, but its moving too fast. Window Size is a problem too

--- src/main/java/edu/pacific/comp55/starter/Environment.java
+++ src/main/java/edu/pacific/comp55/starter/Environment.java
@@ -7,32 +7,54 @@ import acm.graphics.*;
 
 	public class Environment extends GraphicsProgram{
 		
-		GImage cloud = new GImage("media/cumulus-cloud-3.jpg",0,0);
-
-		public void init() {
-			setSize(1920,1080);
-			requestFocus();
-		}
+		
+		int x = 800;
+		int y = 0;
+		int velocity = 75;
+		int BREAK_MS = 30;
+		
+		//placeholder for background
+		//sets location x,y where the image should be "drawn"
+		
+		GImage background = new GImage("media/collection-mountain-scenery-for-game-background-vector-14922765.jpg",0,0);
+		
+		
+		
+		//placeholder for cloud image, trying to make clouds move across.
+		//sets location of x,y where the image should be "drawn"
+		GImage cloud = new GImage("media/cumulus-cloud-3.jpg", x, y);
+		
+		
+		
 		
 		public void run() {
-			cloud.setSize(1920,1080);
+			background.setSize(1000,1000);
+			add(background);
+			cloud.setSize(100,100);
 			add(cloud);
+			moveClouds();
 		}
 		
+	
+	
+//	public void setClouds( ) {
+//		
+//		
+//	}
+	
 		
-	
-	
-	public void setBackground( ){
+	//okay its moving now, but its moving fast, and it seems to be stuck at two portions of the window for some reason.
 		
-	}
-	
-	
-	public void setClouds( ) {
+	public void moveClouds() {
 		
-		
-	}
-	
-	public void moveClouds( ) {
+		while (true){
+			
+			cloud.move(velocity, 0);
+			if(x>0) {
+				x = x - velocity; 
+				velocity *= -1;
+			}
+		}
 		
 	}
 	
