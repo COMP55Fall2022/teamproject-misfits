cc18e57dafdc37ebf86ca5252fd142da02cdbb8d
Santi <85santig@gmail.com>
2022-11-25 19:18:16 -0800

    made enum class for weapons

--- src/main/java/edu/pacific/comp55/starter/Weapons.java
+++ src/main/java/edu/pacific/comp55/starter/Weapons.java
@@ -1,5 +1,15 @@
 package edu.pacific.comp55.starter;
 
-public class Weapons {
+public enum Weapons {
+	SWORD, KINGSWORD, MUSKET;
+	
+	public String toString() {
+		switch(this){
+			case SWORD: return "sword";
+			case KINGSWORD: return "king sword";
+			case MUSKET: return "musket";
+		}
+		return "n/a";
+	}
 
 }
