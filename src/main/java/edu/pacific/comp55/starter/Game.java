fd0908a2d1ceeda4e384628f51bb712cb460d23e
Santi <85santig@gmail.com>
2022-12-07 16:25:19 -0800

    removed init from run

--- src/main/java/edu/pacific/comp55/starter/Game.java
+++ src/main/java/edu/pacific/comp55/starter/Game.java
@@ -24,7 +24,6 @@ package edu.pacific.comp55.starter;
   
   public void run() {
   
-  init(); 
   
   int fps = 60; //number of frames per second. start of GAMELOOP
   double tickPerSecond = 1000000000/fps; 
